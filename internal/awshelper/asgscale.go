34de9530cc76250b423f45eae46a827b85731c7c
Jeffrey Leung <jeffrey.leung@coursehero.com>
2020-08-24 20:54:34 -0700

    remove unused code

---
+++ internal/awshelper/asgscale.go
@@ -0,0 +1,58 @@
+package awshelper
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+)
+
+// AsgScaleDown scale down the ASG by reducing by 50%
+func AsgScaleDown(asgNameList []string, awsProfile []string) {
+	sess := sessionHelper(awsProfile[0])
+	svc := autoscaling.New(sess)
+
+	scaledDownCapacity := int64(0)
+	minSizeInt := int64(0)
+
+	for _, i := range asgNameList {
+		fmt.Printf("Scaling down the AutoScalingGroup: %v to %v.\n", i, scaledDownCapacity)
+		_, errorSetMin := setASGMinSize(i, svc, minSizeInt)
+
+		if errorSetMin != nil {
+			log.Printf("Unexpeced Error: %v\n", errorSetMin)
+			continue
+		}
+
+		_, errorSetDesired := setASGDesiredCap(i, svc, scaledDownCapacity)
+
+		if errorSetDesired != nil {
+			log.Printf("Unexpeced Error: %v\n", errorSetDesired)
+			continue
+		}
+	}
+}
+
+func setASGMinSize(asgName string, svc *autoscaling.AutoScaling, minSizeInt int64) (*autoscaling.UpdateAutoScalingGroupOutput, error) {
+	updateASGInput := &autoscaling.UpdateAutoScalingGroupInput{
+		AutoScalingGroupName: aws.String(asgName),
+		MinSize:              aws.Int64(minSizeInt),
+	}
+
+	result, err := svc.UpdateAutoScalingGroup(updateASGInput)
+
+	return result, err
+}
+
+func setASGDesiredCap(asgName string, svc *autoscaling.AutoScaling, scaledDownInt int64) (*autoscaling.SetDesiredCapacityOutput, error) {
+	desiredCapInput := &autoscaling.SetDesiredCapacityInput{
+		AutoScalingGroupName: aws.String(asgName),
+		DesiredCapacity:      aws.Int64(scaledDownInt),
+		HonorCooldown:        aws.Bool(true),
+	}
+
+	result, err := svc.SetDesiredCapacity(desiredCapInput)
+
+	return result, err
+}
---
+++ internal/awshelper/helper.go
@@ -0,0 +1,97 @@
+package awshelper
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func sessionHelper(awsProfile string) *session.Session {
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Profile: awsProfile,
+		Config: aws.Config{
+			Region: aws.String("us-east-1"),
+		},
+		SharedConfigState: session.SharedConfigEnable,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return sess
+}
+
+// GetAutoScaleGroupList get list of autoscalegroups used by the existing nodegroups
+func GetAutoScaleGroupList(instanceIDList []string, awsProfile []string) []string {
+	sess := sessionHelper(awsProfile[0])
+	var autoScaleGroupNames []string
+
+	for _, instanceID := range instanceIDList {
+		autoScaleGroupNames = append(autoScaleGroupNames, asgLocator(instanceID, sess))
+	}
+
+	cleanedAutoScaleGroupNames := removeDuplicateFromStringSlice(autoScaleGroupNames)
+
+	return cleanedAutoScaleGroupNames
+}
+
+func removeDuplicateFromStringSlice(stringSlice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, i := range stringSlice {
+		if _, value := keys[i]; !value {
+			keys[i] = true
+			list = append(list, i)
+		}
+	}
+	return list
+}
+
+func asgLocator(instanceID string, sess *session.Session) string {
+	svc := ec2.New(sess)
+
+	ec2Input := &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{
+			aws.String(instanceID),
+		},
+	}
+
+	result, err := svc.DescribeInstances(ec2Input)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+
+	for _, reservations := range result.Reservations {
+		for _, tags := range reservations.Instances {
+			for _, kv := range tags.Tags {
+				if aws.StringValue(kv.Key) == "aws:autoscaling:groupName" {
+					return aws.StringValue(kv.Value)
+				}
+			}
+		}
+	}
+	return ""
+}
+
+func asgGetCurrentDesiredCap(asg string, service *autoscaling.AutoScaling) int64 {
+	input := &autoscaling.DescribeAutoScalingGroupsInput{
+		AutoScalingGroupNames: []*string{
+			aws.String(asg),
+		},
+	}
+
+	result, err := service.DescribeAutoScalingGroups(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, i := range result.AutoScalingGroups {
+		return aws.Int64Value(i.DesiredCapacity)
+	}
+
+	return 1
+}
